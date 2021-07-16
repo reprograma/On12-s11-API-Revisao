8535531c82f27e7a6da0021c0be136881f7e0f11
Paula Allemand <paulaalemand@gmail.com>
2021-07-16 19:18:48 -0300

    models adicionados e rotas padrões definidas :thumbsup:

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,10 @@
 const express = require("express");
 const app = express();
 
+const travelsRoutes = require("./routes/travelsRoutes");
+const passangersRoutes = require("./routes/passengersRoutes");
+
+app.use("/travels", travelsRoutes);
+app.use("/passangers", passangersRoutes);
+
 module.exports = app;
