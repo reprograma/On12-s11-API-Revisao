d30770a604640da43f40dadad7ba99ebb8f6ca66
Paula Allemand <paulaalemand@gmail.com>
2021-07-17 10:50:39 -0300

    atualização arquivos :thumbsup

--- backend/src/controllers/travelsControllers.js
+++ backend/src/controllers/travelsControllers.js
@@ -1,5 +1,5 @@
 const travels = require("../models/travels.json");
-const utils = require("../utils/travelsUtils");
+// const utils = require("../utils/travelsUtils");
 
 const getAllTravels = (req, res) => {
 
