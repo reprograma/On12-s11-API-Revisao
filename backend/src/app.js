1b51d01983de3a6b9b3c28deb2217267aed490bd
Paula Allemand <paulaalemand@gmail.com>
2021-07-17 10:45:24 -0300

    atualização arquivos :thumbsup

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,5 @@
 const express = require("express");
 const app = express();
 
-const travelsRoutes = require("./routes/travelsRoutes");
-const passengersRoutes = require("./routes/passengersRoutes");
-
-app.use("/travels", travelsRoutes);
-app.use("/passengers", passengersRoutes);
 
 module.exports = app;
